c5137bf0bc2e0d1d7dcb42494b6861b98ac35bd1
Zachary Carey <zcarey@ltu.edu>
2020-06-04 14:02:31 -0400

    Part 10: Horizontal Intersections

--- raycast.js
+++ raycast.js
@@ -87,17 +87,69 @@ class Player {
 
 class Ray{
     constructor(rayAngle){
-        this.rayAngle = rayAngle;
+        this.rayAngle = normalizeAngle(rayAngle);
+        this.wallHitX = 0;
+        this.wallHitY = 0;
+        this.distance = 0;
+
+        this.isRayFacingDown = this.rayAngle > 0 && this.rayAngle < Math.PI;
+        this.isRayFacingUp = !this.isRayFacingDown;
+
+        this.isRayFacingRight = this.rayAngle < 0.5*Math.PI || this.rayAngle > 1.5*Math.PI;
+        this.isRayFacingLeft = !this.isRayFacingRight;
+    }
+
+    cast(columnId){
+        var xintercept, yintercept;
+        var xstep, ystep;
+
+        /*Horizontal ray-grid intersection code*/
+        var foundHorzWallHit = false;
+        var wallHitX = 0;
+        var wallHitY = 0;
+
+        //Find the y-coordinate of the closest horizontal grid intersection
+        yintercept = Math.floor(player.y / TILE_SIZE) * TILE_SIZE;
+        yintercept += this.isRayFacingDown ? TILE_SIZE : 0;
+
+        //Find the x-coordinate of the closest horizontal grid intersection
+        xintercept = player.x + (yintercept - player.y) / Math.tan(this.rayAngle);
+
+        //Calculate the increment xstep and ystep
+        ystep = TILE_SIZE;
+        ystep *= this.isRayFacingUp ? -1 : 1;
+
+        xstep = TILE_SIZE / Math.tan(this.rayAngle);
+        xstep *= (this.isRayFacingLeft && xstep > 0) ? -1 : 1;
+        xstep *= (this.isRayFacingRight && xstep < 0) ? -1 : 1;
+
+        var nextHorzTouchX = xintercept;
+        var nextHorzTouchY = yintercept;
+
+        if (this.isRayFacingUp) nextHorzTouchY--;
+
+        //Increment xstep and ystep until we find a wall
+        while(nextHorzTouchX >= 0 && nextHorzTouchX <= WINDOW_WIDTH && nextHorzTouchY >= 0 && nextHorzTouchY <= WINDOW_HEIGHT){
+            if(grid.hasWallAt(nextHorzTouchX, nextHorzTouchY)){
+                foundHorzWallHit = true;
+                wallHitX = nextHorzTouchX;
+                wallHitY = nextHorzTouchY;
+                break;
+            }else{
+                nextHorzTouchX += xstep;
+                nextHorzTouchY += ystep;
+            }
+        }
     }
 
     render(){
         stroke('rgba(255, 0, 0, 0.3)');
-        line(
+        /*line(
             player.x, 
             player.y, 
             player.x + Math.cos(this.rayAngle) * 30,
             player.y + Math.sin(this.rayAngle) * 30
-        );
+        );*/
     }
 }
 
@@ -141,11 +193,19 @@ function castAllRays() {
     //for (var i = 0; i < NUM_RAYS; i++){
     for (var i = 0; i < 1; i++){
         var ray = new Ray(rayAngle);
-        //ray.cast();
+        ray.cast(columnId);
         rays.push(ray);
         rayAngle += FOV_ANGLE / NUM_RAYS;
         columnId++;
     }
+}
+
+function normalizeAngle(angle){
+    angle = angle % (2 * Math.PI);
+    if(angle < 0){
+        angle = (2 * Math.PI) + angle;
+    }
+    return angle;
 }
 
 function setup() {
